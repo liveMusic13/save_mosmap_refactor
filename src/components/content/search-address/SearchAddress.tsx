cbd7d4c39eabed96e1f2f78f965b75f823a23105
Dmitriy <aegis.ko20@mail.ru>
2024-11-19 12:37:26 +0300

    add mini-fix

--- src/components/content/search-address/SearchAddress.tsx
+++ src/components/content/search-address/SearchAddress.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, FC, useEffect, useState } from 'react'
+import { ChangeEvent, FC, useEffect, useRef, useState } from 'react'
 
 import { mapService } from '@/services/map.service'
 import { actions as dataObjectsInMapAction } from '@/store/data-objects-in-map/dataObjectsInMap.slice'
@@ -7,57 +7,6 @@ import { IDataSearchAddress } from '@/types/data.types'
 import { useDispatch } from 'react-redux'
 import styles from './SearchAddress.module.scss'
 
-// const SearchAddress: FC = () => {
-//   const dispatch = useDispatch()
-//   const [value, setValue] = useState<string>('')
-//   const [dataResponse, setDataResponse] = useState<IDataSearchAddress | any>({url: '', list: []})
-
-//   const onChange = (e: ChangeEvent<HTMLInputElement>) => setValue(e.target.value)
-
-//   const getData = async () => setDataResponse(await mapService.getHelpSearchAddress(dispatch, value))
-
-//   useEffect(()=> {
-//     if (value.length > 2) getData()
-//   }, [value])
-
-//   const extractCityAndStreet = (address: string): string => { // Разбиваем строку на части по запятой 
-//     const parts = address.split(', '); // Получаем первое слово и первые два слова после запятой 
-//     const city = parts[0]; const streetParts = parts[1].split(' ').slice(0, 2).join(' '); // Формируем и возвращаем новую строку 
-//     return `${city}, ${streetParts}`;
-//   }
-
-//   const onClick = async (e:any) => {
-//     const response = await mapService.getHelpSearchAddress(dispatch, e.target.innerText)
-//     setDataResponse(response)
-//     console.log('response.list[0].length: ', response.list.length)
-  
-//     // setValue(extractCityAndStreet(response.list[0].name))
-//     if (response.list.length === 1) {
-//       dispatch(dataObjectsInMapAction.addNewCenter(response.list[0].coords))
-//       dispatch(viewSettingsAction.toggleSearchAddress(''))
-//     } 
-//     // else if (response.list.length > 0) {
-//     //   // setValue(extractCityAndStreet(response.list[0].name))
-//     //   setValue(response.list[0].name)
-//     // }
-//   }
-
-//   return (
-//     <div className={styles.block__searchAddress}>
-//       <input type="text" className={styles.input} onChange={onChange} value={value} placeholder='Введите адрес дома' />
-//       {dataResponse.list.length > 0 && (
-//         <div className={styles.block__variables}>
-//           {
-//             dataResponse?.list.map((el:any) => (
-//               <p key={el.id} className={styles.variables} onClick={onClick}>{el.name}</p>
-//             ))
-//           }
-//         </div>
-//       )}
-//     </div>
-//   )
-// }
-
 const SearchAddress: FC = () => {
   const dispatch = useDispatch();
   const [value, setValue] = useState<string>('');
@@ -65,6 +14,7 @@ const SearchAddress: FC = () => {
     url: '',
     list: [],
   });
+  const inputRef = useRef<HTMLInputElement>(null);
 
   // Обработчик изменения инпута
   const onChange = (e: ChangeEvent<HTMLInputElement>) => setValue(e.target.value);
@@ -83,18 +33,29 @@ const SearchAddress: FC = () => {
     }
   }, [value]);
 
+  useEffect(() => { 
+    if (inputRef.current) { 
+      setTimeout(() => { 
+        inputRef.current?.focus(); // Устанавливаем фокус в инпут 
+      }, 0); 
+    } 
+  }, [dataResponse]);
+
   // Обработчик нажатия на элемент списка
   const onClick = async (e: any) => {
     const selectedValue = e.target.innerText;
 
-    // Устанавливаем значение выбранного элемента в инпут
-    setValue(selectedValue);
+    // Устанавливаем значение выбранного элемента в инпут и добавляем пробел 
+    setValue((prevValue) => `${selectedValue} `)
 
     // Отправляем запрос с выбранным значением
     const response = await mapService.getHelpSearchAddress(dispatch, selectedValue);
     setDataResponse(response);
 
-    if (response.list.length === 1) {
+    const filterValue =  response.list.filter((el:any) => el.name === selectedValue) 
+    console.log(response)
+    
+    if (filterValue.length > 0 && selectedValue === filterValue[0].name && filterValue[0].coords) {
       dispatch(dataObjectsInMapAction.addNewCenter(response.list[0].coords));
       dispatch(viewSettingsAction.toggleSearchAddress(''));
     }
@@ -103,6 +64,7 @@ const SearchAddress: FC = () => {
   return (
     <div className={styles.block__searchAddress}>
       <input
+        ref={inputRef}
         type="text"
         className={styles.input}
         onChange={onChange}
